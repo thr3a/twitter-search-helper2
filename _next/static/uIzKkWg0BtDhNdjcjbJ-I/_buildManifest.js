a85234fb8984ddf240e6f27abe71687297a51b35
thr3a <thr3a@users.noreply.github.com>
2022-10-18 13:25:28 +0000

    deploy: 9790fa1c8ea46329a1dc6e3347c82b1506202556

--- _next/static/uIzKkWg0BtDhNdjcjbJ-I/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/ef6529d7-b3892274e7bb563a.js","static/chunks/246-ff2d46b0387b30f6.js","static/chunks/pages/index-f0555334f8b00cac.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
