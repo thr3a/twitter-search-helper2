1e374878e600bdf8795fc81182d3e3a2d7344c0c
thr3a <thr3a@users.noreply.github.com>
2023-02-19 18:03:36 +0000

    deploy: 39263b4873600b5a7483b300cfac8b3915026c11

---
+++ _next/static/poWS--kPcmAcv__Hyj_OF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/ef6529d7-b3892274e7bb563a.js","static/chunks/4-33f2439877522d46.js","static/chunks/pages/index-97272c1ad1b8bf85.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
